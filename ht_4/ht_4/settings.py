f0611c01bf01b5f5b9c6c9002ff566ef43d90235
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2021-08-16 12:35:07 +0300

    app_creation

--- ht_4/ht_4/settings.py
+++ ht_4/ht_4/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',  # Фреймворк статическими файлами
 
     'polls.apps.PollsConfig',  # Учебное приложение
+    'triangle.apps.PollsConfig',  # Приложение для треугольника
 ]
 
 MIDDLEWARE = [
