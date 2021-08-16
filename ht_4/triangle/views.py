1a2669806d73b89b24ca66dbf3c43ecdccde689f
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2021-08-17 00:33:45 +0300

    прикручиваем шаблон

--- ht_4/triangle/views.py
+++ ht_4/triangle/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
+#  from django.http import HttpResponse
+from django.shortcuts import render
+
+'''
+def index(request):
+    return HttpResponse("<p>Аллиуйа, Аминь. Надо было убрать скобки после переменной  "
+                        "views.index в файле urls.py </p>")
+
+def title(request):
+    return HttpResponse('<h2>Страница треугольника</h2>')
+'''
 
 
 def index(request):
-    return HttpResponse("Аллиуйа, Аминь. Я так и не понял, что было не так. "
-                        "Похоже, что порядок URL перечисленный в файле urls.py "
-                        "папки проекта имел значение.")
+    side_a = 4
+    side_b = 4
+    tri_type = ['прямоугольный', 'равносторонний', 'равнобедренный']
+    tt = str()
+    if side_a == side_b:
+        tt = tri_type[2]
+    else:
+        tt = tri_type[0]
+    data = {'text': 'Добрый вечер', 'side_a': side_a,
+            'side_b': side_b, 'tri_type': tt}
+    return render(request, 'index.html', context=data)
