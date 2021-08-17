1002f645002371e6c5b0398151b7d4dd90e668fb
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2021-08-17 17:01:48 +0300

    forma with two fields without submot button

---
+++ ht_4/triangle/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class Triangle(forms.Form):
+    side_1 = forms.IntegerField(label='Сторона 1', min_value=1)
+    side_2 = forms.IntegerField(label='Сторона 2', min_value=1)
+    #  ok = forms.
--- ht_4/triangle/views.py
+++ ht_4/triangle/views.py
@@ -1,6 +1,8 @@
 #  from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import Triangle
+
 '''
 def index(request):
     return HttpResponse("<p>Аллиуйа, Аминь. Надо было убрать скобки после переменной  "
@@ -15,11 +17,12 @@ def index(request):
     side_a = 4
     side_b = 4
     tri_type = ['прямоугольный', 'равносторонний', 'равнобедренный']
+    form = Triangle()
     tt = str()
     if side_a == side_b:
         tt = tri_type[2]
     else:
         tt = tri_type[0]
     data = {'text': 'Добрый вечер', 'side_a': side_a,
-            'side_b': side_b, 'tri_type': tt}
+            'side_b': side_b, 'tri_type': tt, 'tr_form': form}
     return render(request, 'index.html', context=data)
